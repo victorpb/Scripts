4d653bbf19370c06519cdb77b32ada4afd804688
victtor.vpb@gmail.com <victtor.vpb@gmail.com>
2015-03-30 19:50:31 -0300

    Finalização da lógica para alteração do gt

--- red_z.py
+++ red_z.py
@@ -1,25 +1,37 @@
+# coding=UTF-8
+
 import 	MySQLdb
 
-con = MySQLdb.connect(user='root', passwd='root',db='red_z')
+user_banco = raw_input('Digite o usuário do banco: ')
 
-#id_caixa_set = input("Digite o ID do caixa: ")
-def update_banco():
+senha = raw_input('Digite a senha do banco: ')
+
+banco = raw_input('Digite o nome do banco de dados: ')
+
+ip_servidor = raw_input('Digite o ip da máquina do MySQL: ')
+
+id_caixa = input('Digite o ID do caixa: ')
+#string_conexao_banco = ('user = %s , passwd = %s , db = %s, host = %s'%(user_banco, senha, banco, ip_servidor)
+
+con = MySQLdb.connect(user = user_banco, passwd = senha, db = banco, host = ip_servidor)
+
+def update_banco(id_caixa, banco):
 
 	c = con.cursor()
 
-	gt_final = c.execute('select grande_total_final from red_z.reducaoz where caixa_caixa_id_oid = 18')
+	gt_final = c.execute('select grande_total_final from %s.reducaoz where caixa_caixa_id_oid = %i' %(banco, id_caixa))
 	lista_gt_final =  c.fetchall()
-	id_red =  c.execute('select reducaoz_id from red_z.reducaoz where caixa_caixa_id_oid = 18')
+	id_red =  c.execute('select reducaoz_id from %s.reducaoz where caixa_caixa_id_oid = %i' %(banco,id_caixa))
 	lista_reducaoz_id = c.fetchall()
 
 	cont_gt_final = 0
 	cont_id_reducaoz = 1
-	'''
-	print len(lista_gt_final)
-	print lista_reducaoz_id
-	'''
+	
+	#print len(lista_gt_final)
+	#print lista_reducaoz_id
+	
 	for cont_gt_final in range(len(lista_gt_final)-1):
-		update = 'update red_z.reducaoz set grande_total_inicial =%f  where reducaoz_id = %i'%(lista_gt_final[cont_gt_final][0], lista_reducaoz_id[cont_id_reducaoz][0])
+		update = 'update %s.reducaoz set grande_total_inicial =%f  where reducaoz_id = %i'%(banco, lista_gt_final[cont_gt_final][0], lista_reducaoz_id[cont_id_reducaoz][0])
 		c.execute(update)
 		con.commit()
 
@@ -30,5 +42,5 @@ def update_banco():
 #c.execute('update red_z.reducaoz set grande_total_inicial = %f where reducaoz_id = 6'%g)
 #con.commit()
 
-update_banco()
+update_banco(id_caixa,banco)
 
