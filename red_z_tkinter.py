9b02206e6e54ecb88d5acba07290f7c8445702cf
victtor.vpb@gmail.com <victtor.vpb@gmail.com>
2015-03-30 23:42:36 -0300

    commit retirada de código de teste

--- red_z_tkinter.py
+++ red_z_tkinter.py
@@ -1,18 +1,9 @@
 # coding=UTF-8
 from Tkinter import *
-import 	MySQLdb
+import MySQLdb
 from datetime import datetime
 
 
-class Janela:
-	def __init__(self,toplevel):
-		self.fr1 = Frame(toplevel)
-		self.fr1.pack()
-		self.botao = Button(self.fr1, text='Oi!', background='green')
-		self.botao.pack()
-		raiz=Tk()
-		Janela(raiz)
-		raiz.mainloop()
 
 class Passwords:
 
